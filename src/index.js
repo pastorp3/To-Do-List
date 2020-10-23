1a5b61b8371589d97fa605a8950ed519e74c0d1d
Jose Pedraza <pastorpedraza3@gmail.com>
2020-10-22 22:39:06 -0500

    save function working only with add project button

--- src/index.js
+++ src/index.js
@@ -9,13 +9,21 @@ const addproject = document.querySelector('.addporjectbutton');
 const form = document.getElementsByTagName('form')[0];
 const text = document.querySelector('.input');
 let todosview = new Project('Todos');
+const datasaved = JSON.parse(localStorage["library"]);
+
+if(datasaved.myprojects.length > 0) {
+	for(let i = 0; i < datasaved.myprojects.length; i++) {
+		console.log(datasaved.myprojects[i]);
+		library.addProject(datasaved.myprojects[i]);
+	};
+}else {
+	library.addProject(todosview);
+};
 
 
 
-library.addProject(todosview);
 
-
-console.log(library);
+console.log(datasaved);;
 
 
 
